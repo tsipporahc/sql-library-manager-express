3455c9d89918b17e1fc2d26da860c013358ccc40
tsipporahc <tsipporahc@gmail.com>
2022-11-07 08:51:23 -0800

    Create Book model

---
+++ models/book.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Book.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { 
+        notEmpty: true,
+        // custom error message
+        msg: 'Please provide a value for "title"',
+       },
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { 
+        notEmpty: true,
+        // custom error message
+        msg: 'Please provide a value for "author"',
+       },
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
